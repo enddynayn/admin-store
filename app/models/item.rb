0f34e691751479b85d2d126ef5eec47a702b126e
Enddy Dumbrique <enddynayn@hotmail.com>
2015-06-16 11:23:06 -0700

    Add class method to find exact number of matches for items that have the same categories.

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,12 @@ class Item < ActiveRecord::Base
   has_many :categories_items, primary_key: 'item_id', foreign_key: 'item_id'
   has_many :categories, through: :categories_items
 
+
+   def self.match_all_categories(category_ids)
+      select(:item_id).joins(:categories_items)
+        .where('categories_items.category_id': category_ids )
+        .group('items.item_id')
+        .having('count(1) = ?', category_ids.length)
+   end
+
 end
